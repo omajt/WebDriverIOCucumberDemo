195f9fb6f3349a54b22ed88ffd07221a47dd5c7c
omajt <83719469+omajt@users.noreply.github.com>
2022-02-15 13:46:26 -0600

    Add some steps to Scenario 3

---
+++ features/step-definitions/product.step.js
@@ -0,0 +1,18 @@
+import {Given, When, Then, And} from "@cucumber/cucumber";
+import productPage from "../pageobjects/product.page";
+
+When(/^I add below products to cart$/, async (table) => {
+       await productPage.addProducts(table);
+});
+Then(/^I shall validate shopping cart as below$/, async (table) => {
+       await productPage.verifyShoppingCart(table);
+});
+Then(/^I am on the contact page$/, async () => {
+       await expect(HomePage.link_Contact).toBePresent();
+       await HomePage.link_Contact.click();
+       await commonPage.vaerifyPageHeading("CUSTOMER SERVICE - CONTACT US");
+});
+     
+And(/^I send refund request to customer care for prev. order$/, async () => {
+       await conatctPage.sendMessageToCustomerCare();
+});
