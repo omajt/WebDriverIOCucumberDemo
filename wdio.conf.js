ce23d0800eac841eb960c303dc0b69a7e1f558e8
Eric Poe <eric.poe@gmail.com>
2022-03-05 20:15:07 -0600

    Remove MaxInstances from webDriver capabilities
    This is not one of the capabilities. See: https://www.w3.org/TR/webdriver/#capabilities

--- wdio.conf.js
+++ wdio.conf.js
@@ -61,11 +61,6 @@ exports.config = {
     //
     capabilities: [
         {
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
-            maxInstances: 5,
-            //
             browserName: "chrome",
             acceptInsecureCerts: true,
             // If outputDir is provided WebdriverIO can capture driver session logs
